4dbac36d8c6ba6148bc17422b39db762c8e415a9
felix <sparkyxu@hotmail.com>
2018-10-23 21:03:32 -0700

    update 9pm before the class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
 $(document).ready(function () {
 
-  // js code
+  $('.loading').hide()
+
+  $(".selection").on("change", function () {
+    const selected = $(this).val();
+    $('.loading').show()
+
+    var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
+    url += '?' + $.param({
+      'api-key': "5d68eebb4fa44bd381bd7fe2988a363d"
+    });
+    $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'json'
+      })
+
+      .done(function (data) {
+        console.log(data);
+        $('.selectPage').empty();
+
+        const titles = data.results;
+        for (let i = 0; i < 12; i++) {
+          console.log(titles[i]);
+          let abstract = titles[i].abstract;
+          let articleImage = titles[i].multimedia[4].url;
+          let links = titles[i].url;
+
+          // console.log(abstract);
+          // console.log(articleImage);
+          console.log(links);
+
+
+          $('.selectPage').append('<a href="' + links + '" class="pictures" style="background:url(' + articleImage + ')">' + '<h2>' + abstract + '</h2>' + '</a>');
+        }
+
+      }).always(function () {
+        $('.loading').hide();
+
+
+      });
+
+  });
+
 
 });
