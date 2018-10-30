b0a630b6ffec096cf3a2cc921a737c463339004d
felix <sparkyxu@hotmail.com>
2018-10-30 13:59:15 -0700

    update

---
+++ js/ch.js
@@ -0,0 +1,60 @@
+// set some inital variables
+const $loader = $('ajx-loader');
+const $stores = $('.selectPage');
+
+$('#sections').on('change', function () {
+            //get the selet value
+            const section = $(this).val();
+
+            if (!section.length) {
+                returen;
+            }
+            //clear out the old stories
+            $stores.empty();
+            //show the loader gif
+            $loader.show();
+            //get stories from the API
+
+            getStories($stories, $loader, section);
+
+            var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
+            url += '?' + $.param({
+                'api-key': "5d68eebb4fa44bd381bd7fe2988a363d"
+            });
+
+            $.ajax - loader({
+                    url: url,
+                    method: 'GET',
+                    dataType: 'json'
+                })
+
+                .done(function (data) {
+
+                    $('.selectPage').empty();
+
+                    const titles = data.results;
+
+                    for (let i = 0; i < 12; i++) {
+
+                        let abstract = titles[i].abstract;
+                        let articleImage = titles[i].multimedia[4].url;
+                        let links = titles[i].url;
+
+                        $('.selectPage').append('<a href="' + links + '" class="pictures" style="background:url(' + articleImage + ')">' + '<h2>' + abstract + '</h2>' + '</a>');
+                    }
+
+                }).always(function () {
+
+
+                }); //end on change
+            //function to get stories from NYT API
+
+
+
+
+
+
+
+
+
+            //function getStories($stories, $loader, section) {}
--- js/script.js
+++ js/script.js
@@ -1,47 +1,56 @@
 $(document).ready(function () {
 
-  $('.loading').hide()
 
-  $(".selection").on("change", function () {
-    const selected = $(this).val();
-    $('.loading').show()
+  // set some inital variables
+  const $loader = $('ajx-loader');
+  const $stores = $('.selectPage');
+
+  $('#sections').on('change', function () {
+    //get the selet value
+    const section = $(this).val();
+
+    if (!section.length) {
+      returen;
+    }
+    //clear out the old stories
+    $stores.empty();
+    //show the loader gif
+    $loader.show();
+    //get stories from the API
+
+    getStories($stories, $loader, section);
 
     var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
     url += '?' + $.param({
       'api-key': "5d68eebb4fa44bd381bd7fe2988a363d"
     });
-    $.ajax({
+
+    $.ajax - loader({
         url: url,
         method: 'GET',
         dataType: 'json'
       })
 
       .done(function (data) {
-        console.log(data);
+
         $('.selectPage').empty();
 
         const titles = data.results;
+
         for (let i = 0; i < 12; i++) {
-          console.log(titles[i]);
+
           let abstract = titles[i].abstract;
           let articleImage = titles[i].multimedia[4].url;
           let links = titles[i].url;
 
-          // console.log(abstract);
-          // console.log(articleImage);
-          console.log(links);
 
 
-          $('.selectPage').append('<a href="' + links + '" class="pictures" style="background:url(' + articleImage + ')">' + '<h2>' + abstract + '</h2>' + '</a>');
+          $('.selectPage').append('<a href="' + links + '" class="pictures" style="background:url(' + articleImage + '); background-size:cover">' + '<h2>' + abstract + '</h2>' + '</a>');
         }
 
       }).always(function () {
-        $('.loading').hide();
 
 
       });
-
   });
-
-
 });
